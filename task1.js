854ca42337de126324854fb557426677138bc104
Helena-Bartonickova <helena.cervin@seznam.cz>
2024-10-08 17:59:53 +0200

    Oprava indexu- přidána const i do task1.js

--- task1.js
+++ task1.js
@@ -24,11 +24,10 @@ const countries = [
 function checkCountry(country, countries){
     if (typeof country !== "string" || country.length === 0) {
       console.log(`Zadali jste neplatný dotaz ${country}`);
-
-  return country;
-  }
-    
-    if (countries.includes(country); const i = countries.indexOf(country) ) {
+    return country;
+    }
+const i = countries.indexOf(country);
+    if (i !== -1) {
         console.log(`Zadaná krajina ${country} se nachází v seznamu krajin na indexe ${i}`)
   } else {
         console.log(`Zadaná krajina ${country} se nenachází v seznamu krajin`)
