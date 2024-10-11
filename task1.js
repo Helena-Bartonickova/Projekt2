d5e695646381aedd68fe32cce6d99c66748aca6f
Helena-Bartonickova <helena.cervin@seznam.cz>
2024-10-11 11:41:59 +0200

    Funkce addCountry je funkční - task1.js

--- task1.js
+++ task1.js
@@ -35,19 +35,16 @@ function checkCountry(country, countries){
     }
   }
 
-
-//2. část
-const arrayLength = countries.length; 
-
 function addCountry(country, countries){
     if (typeof country !== "string" || country.length === 0) {
       console.log(`Zadali jste neplatný dotaz ${country}`);
     return country;
     }
-  const i = countries.indexOf(country);
-  countries.push(country);  
-  if (i === -1) {
-      console.log(`Zadaná krajina ${country} byla úspěšně přidána do seznamu krajin na indexu ${i}. Celkový počet krajin v seznamu je ${arrayLength}.`);
+      const i = countries.indexOf(country);  
+    if (i === -1) {
+      countries.push(country);
+      const newI = countries.length - 1;
+      console.log(`Zadaná krajina ${country} byla úspěšně přidána do seznamu krajin na indexu ${newI}. Celkový počet krajin v seznamu je ${countries.length}.`);
   } else {
       console.log(`Zadaná krajina ${country} se nachází v seznamu krajin na indexu ${i}`);
     }
